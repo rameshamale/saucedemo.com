d92e12cde7174e1f4cc64f31f3f9f7de9aaeaf9c
Ramesh <rameshamale50@gmail.com>
2024-05-26 14:58:20 +0530

    Buttons test cases are added

--- src/test/java/Tests/BaseTests.java
+++ src/test/java/Tests/BaseTests.java
@@ -9,6 +9,7 @@ import org.testng.annotations.BeforeMethod;
 
 import Pages.AlertsPage;
 import Pages.BrowserWindowPage;
+import Pages.ButtonsPage;
 import Pages.HomePage;
 
 public class BaseTests {
@@ -19,6 +20,7 @@ public class BaseTests {
     public HomePage homepage;
     public BrowserWindowPage browserWindowPage;
     public AlertsPage alertpage;
+    public ButtonsPage buttonsPage;
     public static final String URL = "https://demoqa.com/";
     public static final String URL1="";
     
@@ -42,6 +44,7 @@ public class BaseTests {
         homepage=new HomePage(driver, wait, actions);
         browserWindowPage= new BrowserWindowPage(driver, wait, actions);
         alertpage=new AlertsPage(driver, wait, actions);
+        buttonsPage=new ButtonsPage(driver, wait, actions); 
     
     
     }
