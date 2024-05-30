cccb415449a67f4ad892fe14a3d7be079ce460f7
Ramesh <rameshamale50@gmail.com>
2024-05-30 23:31:04 +0530

    Removed assertion from textbox file

--- src/test/java/Tests/TextBoxTest.java
+++ src/test/java/Tests/TextBoxTest.java
@@ -21,10 +21,6 @@ public class TextBoxTest extends BaseTests {
         textBoxPage.clickSubmit();
         waitTime();
 
-        Assert.assertEquals(textBoxPage.assertionResponse("Name"),"Name:"+f1);
-        Assert.assertEquals(textBoxPage.assertionResponse("Email"),"Email:"+eMail+domain);
-        Assert.assertEquals(textBoxPage.assertionResponse("Current Address"),"Current Address :"+cAdd);
-        Assert.assertEquals(textBoxPage.assertionResponse("Permanent Address"),"Permananet Address :"+pAdd);
     }
 
 }
